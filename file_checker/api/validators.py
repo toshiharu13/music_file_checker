a9f719caeec5b9e688d81cd2a2d0e6978d613a82
Boyko ALexey <elorian@yandex.ru>
2022-12-17 23:46:58 +0300

    Complete, need clear code, make readme

--- file_checker/api/validators.py
+++ file_checker/api/validators.py
@@ -1,7 +1,8 @@
 import os.path
 from django.conf import settings
 
+
 def validate_music_file(filename):
     _, ext = os.path.splitext(filename)
-    print(ext, settings.MUSIC_EXT)
     return ext == settings.MUSIC_EXT
+
--- file_checker/api/views.py
+++ file_checker/api/views.py
@@ -3,23 +3,33 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
+import soundfile as sf
+from django.shortcuts import get_object_or_404
 
 from .serializers import FileSerializer
 from .validators import validate_music_file
+from .models import MusicFile
 
 
 class GetFile(APIView):
     parser_classes = [MultiPartParser, FormParser]
 
     def post(self, request, *args, **kwargs):
-        uploaded_file_ext = request.FILES['file'].name
+        uploaded_file_name = request.FILES['file'].name
         serializer = FileSerializer(data=request.data)
         if serializer.is_valid():
-            if validate_music_file(uploaded_file_ext):
+            if validate_music_file(uploaded_file_name):
                 serializer.save()
-                print(uploaded_file_ext)
-                return Response('Формат корректен', status=status.HTTP_200_OK)
             else: return Response('Не верное расширение файла')
+
+            music_file_object = get_object_or_404(MusicFile, title=uploaded_file_name)
+            music_file_path = music_file_object.file.path
+            try:
+                data, samplerate = sf.read(music_file_path)
+                return Response('Формат корректен', status=status.HTTP_200_OK)
+            except sf.LibsndfileError as err:
+                return Response('Ошибка чтения, проверьте формат и целостность файла')
+
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
